97ab8aa2342c9d57e88ae31291844389f17b1fca
kaizz04 <kaveesh@marketpushapps.com>
2024-09-11 10:38:50 +0530

    Using & connecting multiple cllasses

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 class Product{
-    title='DEFAULT';
-    imageUrl;
-    description;
-    price;
+    // title='DEFAULT';
+    // imageUrl;
+    // description;
+    // price;
 
     constructor(title,image,desc,price){
         this.title = title;
@@ -13,35 +13,60 @@ class Product{
 
 }
 
-const productList = {
-    products:[
-        new Product('BMW m3', 'https://images.unsplash.com/photo-1516610540415-d1b25463c7f3?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Ym13fGVufDB8fDB8fHww','desc',20000.00),
-        new Product('BMW x3', 'https://images.unsplash.com/photo-1510903117032-f1596c327647?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGJtd3xlbnwwfHwwfHx8MA%3D%3D','desc',40000.00),
-        new Product('BMW m4', 'https://images.unsplash.com/photo-1600268330186-76564be81357?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGJtd3xlbnwwfHwwfHx8MA%3D%3D','desc',50000.00)
-    ],
+class ProductItem{
+
+    constructor(product){
+        this.product = product;
+
+    }
+
     render(){
+        const prodEl = document.createElement('li');
+        prodEl.className='product-item';
+        prodEl.innerHTML = `
+        <div>
+        <img src="${this.product.imageUrl}" alt ="${this.product.title}"/>
+        <div class="product-item__content">
+        <h2>${this.product.title}</h2>
+        <h3>\$${this.product.price}</h3>
+        <p>${this.product.description}</p>
+        <button>Add to cart</button>
+        </div>
+        </div>
+    
+        `;
+        return prodEl;
+
+    }
+
+}
+
+class ProductList{
+    
+        products = [
+            new Product('BMW m3', 'https://images.unsplash.com/photo-1516610540415-d1b25463c7f3?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Ym13fGVufDB8fDB8fHww','desc',20000.00),
+            new Product('BMW x3', 'https://images.unsplash.com/photo-1510903117032-f1596c327647?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGJtd3xlbnwwfHwwfHx8MA%3D%3D','desc',40000.00),
+            new Product('BMW m4', 'https://images.unsplash.com/photo-1600268330186-76564be81357?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGJtd3xlbnwwfHwwfHx8MA%3D%3D','desc',50000.00)
+        ]
+ 
+
+    constructor(){}
+    render(){
+
         const renderHook = document.getElementById('app');
         const prodList = document.createElement('ul');
         prodList.className='product-list';
         for(const prod of this.products){
-            const prodEl = document.createElement('li');
-            prodEl.className='product-item';
-            prodEl.innerHTML = `
-            <div>
-            <img src="${prod.imageUrl}" alt ="${prod.title}"/>
-            <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to cart</button>
-            </div>
-            </div>
-        
-            `;
+        const productItem = new ProductItem(prod);
+        const prodEl = productItem.render();
             prodList.append(prodEl);
         }
         renderHook.append(prodList);
-    }
-};
 
+    }
+    
+}
+
+
+const productList = new ProductList();
 productList.render();
